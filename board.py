bfd2659f1bf774fe23dfdc8c6ea5aa310e249759
Yoshi Sato <49881695+yoshiisato@users.noreply.github.com>
2024-01-03 10:38:48 +0900

    Update board.py
    Added description comments

--- board.py
+++ board.py
@@ -1,8 +1,7 @@
-#functions for initializing the board, updating it, checking for wins, etc. 
-#width 7 x height 6
 import constants
 import pygame
 
+#Draws grid on the Pygame GUI using squares
 def drawGrid(screen):
     blocksize = 100
     for x in range(0, constants.SCREEN_WIDTH, blocksize):
@@ -10,7 +9,7 @@ def drawGrid(screen):
             rect = pygame.Rect(x,y,constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT)
             pygame.draw.rect(screen, constants.BLACK, rect, 1)
 
-
+#Draws circles given the column (x) and row (y) numbers
 def drawCircle(screen, x, y, color):
     center_x = 100*x+50
     center_y = 100*y+50
